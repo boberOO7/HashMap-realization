1bb5e1bd237093fce1d76aa0dcee7d0a960d0a24
boberOO7 <vasilios.bober@gmail.com>
2019-06-04 21:29:00 +0300

    Entry was added

---
+++ src/main/java/com/liakhovskyi/hashmap/MyEntry.java
@@ -0,0 +1,38 @@
+package com.liakhovskyi.hashmap;
+
+import java.util.Objects;
+
+public class Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public Entry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entry entry = (Entry) o;
+        return key == entry.key;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key);
+    }
+}
